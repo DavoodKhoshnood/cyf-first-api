2359937c772d641513882c9b14370f3925daa29d
davoodkhoshnood <khoshnood1977@gmail.com>
2022-11-10 16:53:35 +0000

    Update API

--- index.js
+++ index.js
@@ -1,28 +1,33 @@
-const express = require('express'); 
+const express = require('express');
 const fetch = require('node-fetch');
-const app = express();             
-const port = 5000;                  
+const app = express();
+const port = 5000;
 
-let YOUR_NAME = "ekemini"
-let YOUR_LUCKY_NUMBER = "3"
 
-let URL = "http://localhost:5001"
+let YOUR_NAME = "Davood" 
+let YOUR_LUCKY_NUMBER = "7"
 
-app.get('/', (req, res) => {       
-    res.send(`Welcome to ${YOUR_NAME}'s 1st API`);                                                        
+let URL = "http://54.161.206.58"
+
+app.get('/', (req, res) => {
+    res.send(`Welcome to ${YOUR_NAME}'s 1st API`);                                          
 });
 
-app.get('/luckynumber', (req, res) => {       
-    res.send(YOUR_LUCKY_NUMBER);                                                       
+app.get('/luckynumber', (req, res) => {
+    res.send(YOUR_LUCKY_NUMBER);
 });
 
-app.get('/worstfoodfrom1', async (req, res) => {    
+app.get('/worstfoodfrom1', async (req, res) => {
     const response = await fetch(`${URL}/worstfoodfrom2`);
     const body = await response.text();
-   
-    res.send(body);                                                       
+
+    res.send(body);
 });
 
-app.listen(port, () => {          
-    console.log(`Now listening on port ${port}`); 
+app.get('/yourname', (req, res) => {
+        res.send(YOUR_NAME)
+});
+
+app.listen(port, () => {
+    console.log(`Now listening on port ${port}`);
 });
